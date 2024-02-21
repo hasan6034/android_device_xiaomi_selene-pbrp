e99e81de3d98d205e332aabb9fab2e9221f4cbcf
Hasan Basri OY <hasanbasrioy@gmail.com>
2024-02-21 09:22:40 +0300

    Unnecessary comment lines deleted

--- omni_selene.mk
+++ omni_selene.mk
@@ -1,10 +1,3 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
